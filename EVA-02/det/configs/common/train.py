015cb411060a79e51f427df25fad172bf0d286d1
abdulrehman6919 <abdulrehmansamsung6919@gmail.com>
2026-05-07 21:52:09 +0500

    'training_param_added'

--- EVA-02/det/configs/common/train.py
+++ EVA-02/det/configs/common/train.py
@@ -10,7 +10,7 @@ train = dict(
         find_unused_parameters=False,
         fp16_compression=False,
     ),
-    checkpointer=dict(period=2500, max_to_keep=100),  # options for PeriodicCheckpointer
+    checkpointer=dict(period=2500, max_to_keep=1),  # options for PeriodicCheckpointer
     eval_period=2500,
     log_period=20,
     device="cuda",
