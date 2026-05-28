4b96d986e7ca9479a5c037183afb4c00d207683c
abdulrehman6919 <abdulrehmansamsung6919@gmail.com>
2026-05-28 20:14:01 +0500

    'params_added'

--- EVA-02/det/projects/ViTDet/configs/eva2_mim_to_coco/eva2_coco_cascade_mask_rcnn_vitdet_l_4attn_1024_gun_ap50_v5.py
+++ EVA-02/det/projects/ViTDet/configs/eva2_mim_to_coco/eva2_coco_cascade_mask_rcnn_vitdet_l_4attn_1024_gun_ap50_v5.py
@@ -180,7 +180,7 @@ for _bp in model.roi_heads.box_predictors:
 # ---------------------------------------------------------------------------
 optimizer.lr = 2e-5
 
-train.max_iter = 25000
+train.max_iter = 6000
 lr_multiplier = L(WarmupParamScheduler)(
     scheduler=L(CosineParamScheduler)(
         start_value=1.0,
